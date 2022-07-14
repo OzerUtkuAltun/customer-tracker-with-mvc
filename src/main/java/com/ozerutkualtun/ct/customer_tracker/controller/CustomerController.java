4ce7f3976405e59a84fe22d3e49b0fc1d6aa3879
Özer Altun <ozerutkualtun@gmail.com>
2022-07-14 15:46:45 +0300

    dao injected into controller

--- src/main/java/com/ozerutkualtun/ct/customer_tracker/controller/CustomerController.java
+++ src/main/java/com/ozerutkualtun/ct/customer_tracker/controller/CustomerController.java
@@ -1,17 +1,23 @@
 package com.ozerutkualtun.ct.customer_tracker.controller;
 
+import com.ozerutkualtun.ct.customer_tracker.dao.CustomerDao;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/customers")
 public class CustomerController {
+
+    private final CustomerDao customerDao;
 
     @GetMapping("/list")
     public String listCustomers(Model model) {
 
+        model.addAttribute("customers", customerDao.getCustomers());
         return "customer-list";
     }
 
--- src/main/webapp/WEB-INF/view/customer-list.jsp
+++ src/main/webapp/WEB-INF/view/customer-list.jsp
@@ -1,16 +1,9 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: ozeru
-  Date: 14.07.2022
-  Time: 15:01
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
     <title>Title</title>
 </head>
 <body>
-        This is a sample jsp file.
+        ${customers.get(0)}
 </body>
 </html>
