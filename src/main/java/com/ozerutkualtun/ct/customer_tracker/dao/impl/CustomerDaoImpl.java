fcf7896cda41c9318e39bb66e1b7aa58fbcd6eef
Özer Altun <ozerutkualtun@gmail.com>
2022-07-18 10:39:19 +0300

    ordering customer data

--- src/main/java/com/ozerutkualtun/ct/customer_tracker/dao/impl/CustomerDaoImpl.java
+++ src/main/java/com/ozerutkualtun/ct/customer_tracker/dao/impl/CustomerDaoImpl.java
@@ -20,7 +20,7 @@ public class CustomerDaoImpl implements CustomerDao {
     public List<Customer> getCustomers() {
 
         Session currentSession = sessionFactory.getCurrentSession();
-        Query<Customer> query = currentSession.createQuery("from Customer", Customer.class);
+        Query<Customer> query = currentSession.createQuery("from Customer order by lastName", Customer.class);
 
         return query.getResultList();
     }
